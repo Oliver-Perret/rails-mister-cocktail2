555f90c55f5a32d9fa225c6370768030bfe87072
OliverPerret <oliver.perret2@gmail.com>
2018-07-02 12:14:17 +0200

    Add figaro - Protect my secret data in config/application.yml

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,17 @@
+class ReviewsController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+
+    @review = Review.new(review_params)
+    @review.cocktail = @cocktail
+    @review.save
+
+    redirect_to cocktail_path(params[:cocktail_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts "start"
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
+
+array_of_hash = ingredient["drinks"]
+ array_of_hash.each do |element|
+  Ingredient.create(name:element["strIngredient1"])
+end
+puts "finish"
+
+# puts "#{ingredient["drinks"].first["strIngredient1"]}"
+
+# array
+mojito = Cocktail.create(name:"Mojito")
+sex_on_the_beach = Cocktail.create(name:"Sex on the beach")
+
+
+# dose_rhum_for_mojito = Dose.create(description: "6cl", cocktail_id: mojito.id, ingredient_id: Ingredient.first.id )
