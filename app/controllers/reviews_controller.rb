58d876e107b46424bf3bad6bac5da317dca32e45
OliverPerret <oliver.perret2@gmail.com>
2018-07-05 09:39:19 +0200

    add review policy for create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,10 +3,14 @@ class ReviewsController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
 
     @review = Review.new(review_params)
+    @review.user = current_user
     @review.cocktail = @cocktail
+
     @review.save
 
     redirect_to cocktail_path(params[:cocktail_id])
+    authorize @cocktail
+
   end
 
   def review_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_04_154329) do
+ActiveRecord::Schema.define(version: 2018_07_05_073122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,9 @@ ActiveRecord::Schema.define(version: 2018_07_04_154329) do
     t.bigint "cocktail_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["cocktail_id"], name: "index_reviews_on_cocktail_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,4 +72,5 @@ ActiveRecord::Schema.define(version: 2018_07_04_154329) do
   add_foreign_key "doses", "cocktails"
   add_foreign_key "doses", "ingredients"
   add_foreign_key "reviews", "cocktails"
+  add_foreign_key "reviews", "users"
 end
