3c8d368330fd2ebb7abe5f0bb229098e4f009e7a
OliverPerret <oliver.perret2@gmail.com>
2018-07-10 13:30:04 +0200

    designed home

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,3 +12,6 @@ console.log('Hello World from Webpacker')
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
